7cf48c544c91dae69b42717ed77bb6dc74fb5fe5
장 상련 <srjang@taihoinst.com>
2018-07-19 16:37:46 +0900

    11

--- webapps/ROOT/assets/js/ui.js
+++ webapps/ROOT/assets/js/ui.js
@@ -19,7 +19,7 @@ $(function () {
     //챗봇창 상단 생성
     $(".wc-header > span").add(
         "<span class='chatTitle'></span>" +
-        "<span class='chatTitleText'><strong>CJ</strong> ChatBot</span>" +
+        "<span class='chatTitleText'><strong>NH</strong> ChatBot</span>" +
         "<span class='topIcon btnClose'><button class='topIcon03'></button></span>" +
         "<span class='topIcon btnLayer btnLayerFull'><button class='topIcon02'></button></span>" +
         "<span class='topIcon btnMin'><button class='topIcon01'></button></span>").appendTo(".wc-header");
