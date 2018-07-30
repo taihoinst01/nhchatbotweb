de59a9e7f07f28d7096e80adf33d6bdedf6c7ab3
Sinook Kang <sokang337@gmail.com>
2018-07-30 13:25:05 +0900

    update

--- webapps/ROOT/assets/js/ui.js
+++ webapps/ROOT/assets/js/ui.js
@@ -64,6 +64,7 @@ $(function () {
         $('.bot-wrap').hide().removeClass("chatOn").addClass("chatOff");
     });
     $('.btnMin').click(function () {
+        $('.btnTopClose').click();
         $('.wc-chatview-panel').css({ "overflow": "hidden" });
         $('.wc-chatview-panel').animate({ "height": "32px" }, "fast");
         $('.wc-console, wc-message-pane').hide();
@@ -75,43 +76,30 @@ $(function () {
         if ($(this).hasClass('btnLayerMid')) {
             //$('.wc-chatview-panel').css({ "overflow": "visible" });
             $('.wc-chatview-panel').animate({ "height": "528px" }, "fast");
+            $('.popupArea').animate({ 'bottom': '176px' }, "fast");
             $('.wc-console, wc-message-pane').show();
             $('.btnLayer').removeClass('btnLayerMid').addClass('btnLayerFull');
             $('.btnLayer > button').css({ 'display': 'inline-block' }).removeClass('topIcon02-1').addClass('topIcon02');
-            $('.btnMin').css({ 'right': '58px', 'display':'inline-block' });
+            $('.btnMin').css({ 'right': '58px', 'display': 'inline-block' });
         } else {
             $('.wc-chatview-panel').animate({ "height": ($(document).height()) + 'px' }, "fast");
+            $('.popupArea').animate({ 'bottom': ($(window).height() - 352) + 'px' }, "fast");
             $('.btnLayer').removeClass('btnLayerFull').addClass('btnLayerMid');
             $('.btnLayer > button').css({ 'display': 'inline-block' }).removeClass('topIcon02').addClass('topIcon02-1');
         }
     });
 
     //챗봇 메뉴 버튼 동작
-    $('.menuIcon').click(function (){
-        if ($('.menuBox').hasClass("off")) {
-            $('.menuBox').removeClass('off').addClass('on');
-            $('.menuBox').css({ 'display': 'block' });
-        } else {
-            $('.menuBox').removeClass('on').addClass('off');
-            $('.menuBox').css({ 'display': 'none' });
-        }
-    });
-    
-	////챗봇 팝업 동작 (동영상)
- //   $(document).on('click', '.wc-card-play > .non-adaptive-content', function () {
- //       var movPopTitle = $(this).children().eq(1).attr('alt');
- //       $('#movTitle').text(movPopTitle);
- //       var movPopUrl = $(this).children().eq(2).attr('alt');
- //       $('#video').attr('src', movPopUrl);
- //       $('.mov-wrapper').show().animate({ "right": "380px", "opacity": "1", "display": "block" }, "fast").fadeIn("fast");
- //   });
- //   //챗봇 팝업 동작 (이미지)
- //   $(document).on('click', '.wc-card-img > .non-adaptive-content', function () {
- //       var imgPopTitle = $(this).children().eq(1).attr('alt');
- //       $('#imgTitle').text(imgPopTitle);
- //       var imgPopUrl = $(this).children().eq(2).attr('alt');
- //       $('.img-wrapper').show().animate({ "right": "380px", "opacity": "1", "display": "block" }, "fast").fadeIn("fast");
- //   });
+    //$('.menuIcon').click(function (){
+    //    if ($('.menuBox').hasClass("off")) {
+    //        $('.menuBox').removeClass('off').addClass('on');
+    //        $('.menuBox').css({ 'display': 'block' });
+    //    } else {
+    //        $('.menuBox').removeClass('on').addClass('off');
+    //        $('.menuBox').css({ 'display': 'none' });
+    //    }
+    //});
+
     //팝업 닫기
     $('.btnTopClose').click(function () {
         $("#video").attr('src', '');
@@ -181,15 +169,15 @@ $(function () {
 });
 
 //챗봇 메뉴 처음으로 돌아가기
-function viewMenu() {
-    var returnText = "return home";     // 처음으로 돌아가는 텍스트
-    $('div.wc-console').addClass('has-text');
-    $('input[type="text"].wc-shellinput').attr('value', returnText);
-    $('input[type="text"].wc-shellinput').val(returnText);
-    $('label.wc-send').trigger('click');
-    $('input[type="text"].wc-shellinput').attr('value', '');
-    $('input[type="text"].wc-shellinput').val('');
-    $('.wc-console').removeClass('has-text');
-    $('.menuBox').removeClass('on').addClass('off');
-    $('.menuBox').css({ 'display': 'none' });
-}
+//function viewMenu() {
+//    var returnText = "return home";     // 처음으로 돌아가는 텍스트
+//    $('div.wc-console').addClass('has-text');
+//    $('input[type="text"].wc-shellinput').attr('value', returnText);
+//    $('input[type="text"].wc-shellinput').val(returnText);
+//    $('label.wc-send').trigger('click');
+//    $('input[type="text"].wc-shellinput').attr('value', '');
+//    $('input[type="text"].wc-shellinput').val('');
+//    $('.wc-console').removeClass('has-text');
+//    $('.menuBox').removeClass('on').addClass('off');
+//    $('.menuBox').css({ 'display': 'none' });
+//}
